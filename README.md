60403bd253a49ae1db273a1349c152ee565b726a
jasongordon <github@jasongordon.com>
2013-02-18 22:45:24 -0500

    First check in of shipping-harvest

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-This is a collection of my google apps scripts.
+google-apps-script
+========
+
+#### shipping-harvest.js ####
+
+This is a script which reads incoming emails from shippers, parses out who it is being sent to and the tracking number and adds it to a tracking service.
+
+Right now, USPS is the shipper and packagetrackr.com is the tracking service.  Look for unread USPS Click-N-Ship emails in a label called usps and parse out the data and email packagetrackr.  Once we have processed the email, mark it read so we dont process it again.  You would want to set up a time based trigger for all of this.
+
+This could easily be extended for other services.
+
 
---
+++ shipping-harvest.js
@@ -0,0 +1,90 @@
+
+
+function go() {
+  var conversations = GmailApp.search("in:usps is:unread subject:(Click-N-Ship)");
+  var ids = [];
+  var errors = [];
+  var data = [];
+  for (i in conversations) {
+    var messages = conversations[i].getMessages();
+    for (j in messages) {
+      var message = messages[j];
+      var body = message.getBody(); 
+      var info = "From: " + message.getFrom() + " - Subject:" + message.getSubject() + " - Date:" + message.getDate();
+      try{
+        ids.push(message.getId());
+        var shipments = matchUSPSHTML(body);
+        for(s in shipments){
+          data.push(shipments[s]);
+        }
+      }
+      catch(err){
+        var str = "Error parsing email - " + info + " - Error:" + err;
+        Logger.log(str);
+        errors.push(str);
+      }
+      
+    }
+  }
+  var user = Session.getActiveUser().getEmail();
+  if(data.length > 0){
+    
+    var body = createEmailBody(data, "packagetrackr");
+    Logger.log("BODY:\n" + body);
+    GmailApp.sendEmail("track@packagetrackr.com", "Add Packages", body, {bcc: user});
+    for(id in ids){
+      GmailApp.getMessageById(ids[id]).markRead();
+    }
+  }
+  if(errors.length > 0 ){
+    var err = "";
+    for(e in errors){
+      err += errors[e] + "\n";
+    }
+    GmailApp.sendEmail(user, "Shipping harvest error report", err);
+  }
+}
+  
+
+function matchUSPSHTML(data){
+  var out = [];
+  var track_num = data.match( /TrackConfirmAction\Winput\.action\WtLabels\=(\d+)/g);
+  var to = data.match(/Shipped.to.*[\r\n]*.*>([a-zA-Z\s]*)<br>/g);
+  for(i in track_num){
+    var o = new Object();
+    var track = track_num[i].match(/(\d+)/g);
+    Logger.log("Track " + i + ": " + track[0]);
+    var person = to[i].match(/>([a-zA-Z\s]+)<br>/);
+    var myPerson = person[1].replace(/(\r\n|\n|\r)/gm,"")
+    o["number"]=track[0];
+    o["carrier"]="USPS";
+    o["person"]=myPerson;
+    out.push(o);
+    Logger.log("Person " + i + ": " + myPerson);   
+  }
+  //body += formatForPackageTrackr(track[0] , "USPS" , myPerson ) ;
+  return out;
+}
+
+function createEmailBody(data, service){
+  var func;
+  var body = "";
+  if(service == "packagetrackr"){
+    func="formatForPackageTrackr";
+  }
+  else{
+    Logger.log("Unsupported service: " + service);
+  }
+  Logger.log(data);
+  for(d in data){
+    body += this[func](data[d]["number"], data[d]["carrier"], data[d]["person"]);
+  }
+  return body;
+}
+
+
+function formatForPackageTrackr(tracking_num, service, person){
+  return "#:" + tracking_num + " " + service + " " + person + "\n";
+}
+
+
